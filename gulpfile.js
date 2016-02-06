551cccf59f2a967ef36b009157e856ef92043505
NealRame <contact@nealrame.com>
2016-02-06 20:11:24 +0100

    Add babel transformation in the js build pipline.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const path = require('path');
 const source = require('vinyl-source-stream');
 const uglify = require('gulp-uglify');
 
 const source_dir = path.join(__dirname, 'src');
+
+const html_source_path = path.join(source_dir, 'index.html');
 
 const js_source_dir = path.join(source_dir, 'js');
 const js_app_source_path = path.join(js_source_dir, 'main.js');
@@ -15,8 +18,19 @@ const assets_dir = path.join(dest_dir, 'assets');
 const js_app_dest_dir = path.join(assets_dir, 'js');
 
 gulp.task('js', () => {
-    const bundle = browserify(js_app_source_path).bundle();
-    return bundle
+    const bundler = browserify(js_app_source_path, {
+        debug: true,
+        paths: ['node_modules', js_source_dir],
+        transform: [
+            ['babelify', {presets: ["es2015"]}]
+        ]
+    });
+    return bundler
+        .bundle()
+        .on('error', (err) => {
+            gutil.log(err.message);
+            err.stream.end();
+        })
         .pipe(source('snake.js'))
         .pipe(buffer())
         .pipe(uglify())
