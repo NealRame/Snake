85ad56ed2e85314986687cd3289c182dc9b1aaf2
NealRame <contact@nealrame.com>
2016-02-09 21:49:32 +0100

    Snake can now eat food.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import {dispatch} from 'functional';
+import Food from 'food';
 import Snake from 'snake';
 import ui from 'ui';
 
 const snake = Snake({width: ui.screen.width/10, height: ui.screen.height/10}, 20);
+const food = Food({width: ui.screen.width/10, height: ui.screen.height/10});
 
 let pause = false;
+let score = 0;
+let high_score = 0;
 
 ui.on('direction-changed', dispatch(
 	(direction) => {
@@ -26,8 +30,7 @@ ui.on('direction-changed', dispatch(
 	}
 )).on('pause', () => pause = !pause);
 
-function draw_snake() {
-	const screen = ui.screen;
+function draw_snake(screen, snake) {
 	const box = {x: 0, y:0, width: 1, height: 1};
 	screen.push();
 	screen.scale(10);
@@ -40,16 +43,35 @@ function draw_snake() {
 	screen.pop();
 }
 
+function draw_food(screen, food) {
+	const box = {x: 0, y: 0, width: 1, height: 1};
+	screen.push();
+	screen.scale(10);
+	screen.translate(food.position);
+	screen.drawRect(box);
+	screen.pop();
+}
+
 snake.reset();
+food.reset();
 
 function run(ts = 0) {
 	window.requestAnimationFrame(run);
 	ui.screen.clear();
-	draw_snake();
+	if (snake.occupies(food.position)) {
+		score += food.points;
+		high_score = Math.max(score, high_score);
+		ui.setScore(score);
+		ui.setHighScore(high_score);
+		food.reset();
+	}
+	draw_snake(ui.screen, snake);
+	draw_food(ui.screen, food);
 	if (!pause) {
 		snake.step(ts);
 	}
 	if (snake.collides()) {
+		ui.setScore(score = 0);
 		snake.reset();
 	}
 }
