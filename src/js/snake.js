b0eb56da24435b109f96d4ebd91572430a1f34b9
NealRame <contact@nealrame.com>
2016-02-09 20:27:08 +0100

    Snake code refactoring.

--- src/js/snake.js
+++ src/js/snake.js
@@ -1,44 +1,79 @@
-function move({x, y}, direction) {
-	if (direction === 'north') {
-		return {x, y: y - 1};
-	}
-	if (direction === 'east') {
-		return {x: x + 1, y};
-	}
-	if (direction === 'south') {
-		return {x, y: y + 1};
-	}
-	if (direction === 'west') {
-		return {x: x - 1, y};
-	}
-	throw new TypeError('Direction must be one of ["north", "east", "south", "west"]');
-}
+import {dispatch, existy} from 'functional';
 
-export default function Snake({width, height}) {
+const move = dispatch(
+	({x, y}, direction)	=> {
+		if (direction === 'north') {
+			return {x, y: y - 1};
+		}
+	},
+	({x, y}, direction) => {
+		if (direction === 'east') {
+			return {x: x + 1, y};
+		}
+	},
+	({x, y}, direction) => {
+		if (direction === 'south') {
+			return {x, y: y + 1};
+		}
+	},
+	({x, y}, direction) => {
+		if (direction === 'west') {
+			return {x: x - 1, y};
+		}
+	},
+	() => {
+		throw new TypeError(
+			'Direction must be one of ["north", "east", "south", "west"]'
+		);
+	}
+);
+
+export default function Snake({width, height}, speed = 40) {
+	let d_step = 1/speed;
+	let last_ts = 0;
 	let direction = 'east';
-	const segments = [
-		{x: width/2 - 1, y: height/2},
-		{x: width/2,     y: height/2},
-		{x: width/2 + 1, y: height/2}
-	];
+	let segments = [];
 	return {
+		reset() {
+			direction = 'east';
+			segments = [
+				{x: width/2 + 1, y: height/2},
+				{x: width/2,     y: height/2},
+				{x: width/2 - 1, y: height/2}
+			];
+		},
 		collides() {
 			const head = segments[0];
-			return segments.some((segment) =>
-				segment !== head && segment.x === head.x && segment.y === head.y
+			return segments.some((segment, index) =>
+				index > 0 && segment.x === head.x && segment.y === head.y
 			);
 		},
-		step() {
-			segments.pop();
-			segments.unshift(move(segments[0], direction));
+		step(ts) {
+			if ((ts - last_ts)/1000 > d_step) {
+				let head = segments[0];
+				if (existy(head)) {
+					head = move(head, direction);
+					head.x = (head.x + width)%width;
+					head.y = (head.y + height)%height;
+					segments.pop();
+					segments.unshift(head);
+				}
+				last_ts = ts;
+			}
 			return this;
+		},
+		get head() {
+			const head = segments[0];
+			return {
+				x: head.x,
+				y: head.y
+			};
 		},
 		get direction() {
 			return direction;
 		},
 		set direction(d) {
 			direction = d;
-			return this;
 		},
 		[Symbol.iterator]: function*() {
 			for (let segment of segments) {
