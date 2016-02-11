edf692b3dcf19fc38e52bc601b391a8fc1a2b702
NealRame <contact@nealrame.com>
2016-02-11 22:32:39 +0100

    Add showMessage() and hideMessage() methods.

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,6 +1,7 @@
 import Screen from 'screen';
 import keyboard from 'keyboard';
 
+const message_ui = document.getElementById('message-box');
 const score_ui = document.getElementById('score');
 const high_score_ui = document.getElementById('high-score');
 const screen = Screen(document.getElementById('screen'));
@@ -18,5 +19,22 @@ export default {
 	setHighScore(v) {
 		high_score_ui.innerHTML = `${v}`;
 		return this;
+	},
+	showMessage(text, title = '') {
+		message_ui.innerHTML = '';
+		if (title) {
+			const h1 = document.createElement('h1');
+			h1.innerHTML = title;
+			message_ui.appendChild(h1);
+		}
+		if (text) {
+			const span = document.createElement('span');
+			span.innerHTML = text;
+			message_ui.appendChild(span);
+		}
+		message_ui.dataset.visible = 'yes';
+	},
+	hideMessage() {
+		message_ui.dataset.visible = 'no';
 	}
 };
