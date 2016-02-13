00b773e86cd0a805b805feb4169b592f37819836
NealRame <contact@nealrame.com>
2016-02-13 16:29:18 +0100

    Add config module.

---
+++ src/js/config.js
@@ -0,0 +1,25 @@
+export default {
+	game: {
+		borders: true,
+		scale: 1/10
+	},
+	food: {
+		points: {
+			easy: 5,
+			normal: 10,
+			hard: 20
+		}
+	},
+	snake: {
+		grow: 1,
+		speed: {
+			easy: 10,
+			normal: 20,
+			hard: 30
+		}
+	},
+	screen: {
+		height: 500,
+		width: 500
+	}
+};
